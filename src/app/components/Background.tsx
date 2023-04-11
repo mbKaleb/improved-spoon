be11be5734d8dccdb9a5a3234917772da6ac12ad
Kaleb Franken <kaleb.franken7@gmail.com>
2023-04-11 17:29:19 -0600

    commit for computer restart

--- src/app/components/Background.tsx
+++ src/app/components/Background.tsx
@@ -1,17 +1,8 @@
-"use client"
-
-import { useState } from "react"
-
-
 
 export default function Background() {
-    const [lineData, setlineData] = useState({
-
-    })
-
     return (
-    <div className="outline absolute top-0 w-full h-full z-0  bg-yellow-200">
-        <p className="h-[240px] w"> </p>
+    <div className="outline absolute top-0 w-full h-full z-0  bg-yellow-200 backdrop-blur ">
+        <p className="h-[240px] w"> TEST </p>
     </div>
   )
 }
--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -2,8 +2,8 @@ import Image from "next/image"
 
 export default function Header() {
 	return (
-		<header className="flex justify-center h-24 p-5 bg-black sticky top-0 z-50">
-			<Image className="margin-left-auto -top-10 absolute" src="/../public/amazon-basics-logo.png" alt="amazon basics logo" width={'200'} height={'5'} />
+		<header className="flex justify-center h-16 p-5 bg-black sticky top-0 z-50">
+			<Image className="margin-left-auto -top-14 absolute" src="/../public/amazon-basics-logo.png" alt="amazon basics logo" width={'200'} height={'5'} />
 			{/* <Link href="/" className="px-2 py-1 bg-white font-bold rounded-lg">Home</Link> */}
 		</header>
 	)
